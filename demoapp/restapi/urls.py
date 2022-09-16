9440c59c7fde01ad4577ed0d9de09dad81965524
robalb <alberto.ventafridda@gmail.com>
2022-09-16 20:23:05 +0200

    added landing page

--- demoapp/restapi/urls.py
+++ demoapp/restapi/urls.py
@@ -1,24 +1,27 @@
 from django.urls import path
 from restapi import views
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
+    # Rest API endpoints
     path('guestnotes/', views.GuestnoteList2.as_view()),
     path('guestnotes/<int:pk>/', views.guestnote_detail),
 
+    # Live openapi specification yaml
     path('openapi', get_schema_view(
-          title="Your Project",
-          description="API for all things …",
+          title="Demoapp DRF",
+          description="A simple rest api written using DRF, with the goal of testing it's introspection algoritm",
           version="1.0.0"
           ), name='openapi-schema'),
+    # Swagger ui for the openapi specification
     path('swagger-ui', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url': 'openapi-schema'}
-    ))
+    ), name="swagger-ui"),
+    # Demo app homepage
+    path('', TemplateView.as_view(
+        template_name='homepage.html',
+        extra_context={'schema_url': 'openapi-schema', 'swagger_url': 'swagger-ui'}
+    )),
 ]
-
-# with this line, django will aknowledge the url patter variations,
-# adding them to the openapi docs
-# urlpatterns = format_suffix_patterns(urlpatterns)
