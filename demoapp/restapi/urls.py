07083b226d84181e58e9da8993e62fd823828cc1
robalb <alberto.ventafridda@gmail.com>
2022-09-19 01:35:58 +0200

    get_schema_view override

--- demoapp/restapi/urls.py
+++ demoapp/restapi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from restapi import views
 from django.views.generic import TemplateView
-from rest_framework.schemas import get_schema_view
+from restapi.openapi.core import get_schema_view
 
 urlpatterns = [
     # Rest API endpoints
@@ -9,11 +9,7 @@ urlpatterns = [
     path('guestnotes/<int:pk>/', views.guestnote_detail),
 
     # Live openapi specification yaml
-    path('openapi', get_schema_view(
-          title="Demoapp DRF",
-          description="A simple rest api written using DRF, with the goal of testing it's introspection algoritm",
-          version="1.0.0"
-          ), name='openapi-schema'),
+    path('openapi', get_schema_view(), name='openapi-schema'),
     # Swagger ui for the openapi specification
     path('swagger-ui', TemplateView.as_view(
         template_name='swagger-ui.html',
